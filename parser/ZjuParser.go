5f9a965c3c23887e12a9627e09a8f3f76b144aaf
Myth <lxbpxylps@126.com>
2023-02-04 00:36:05 +0800

    fix: route all zju.edu.cn traffic

--- parser/ZjuParser.go
+++ parser/ZjuParser.go
@@ -1,9 +1,11 @@
 package parser
 
+import "EasierConnect/core/config"
+
 func ParseZjuDnsRules(debug bool) {
 
 }
 
 func ParseZjuForceProxyRules(debug bool) {
-
+	config.AppendSingleZjuForceProxyRule("zju.edu.cn", debug)
 }
