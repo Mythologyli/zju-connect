e01146cf0bdaa1caa1b77bc19c3c3157a84e8a90
Peisheng Li <lipeisheng2@gmail.com>
2025-06-14 14:05:19 +0800

    fix: keep alive

--- service/keep_alive.go
+++ service/keep_alive.go
@@ -4,37 +4,45 @@ import (
 	"context"
 	"github.com/mythologyli/zju-connect/log"
 	"github.com/mythologyli/zju-connect/resolve"
-	"net"
 	"time"
 )
 
 func KeepAlive(resolver *resolve.Resolver) {
-	var remoteTCPResolver *net.Resolver
 	remoteUDPResolver, err := resolver.RemoteUDPResolver()
 	if err != nil {
-		log.DebugPrintf("KeepAlive: %s", err)
+		log.Printf("KeepAlive: %s", err)
+	}
 
-		remoteTCPResolver, err = resolver.RemoteTCPResolver()
-		if err != nil {
-			log.Printf("KeepAlive: %s", err)
-			panic("KeepAlive: " + err.Error())
-		}
+	remoteTCPResolver, err := resolver.RemoteTCPResolver()
+	if err != nil {
+		log.Printf("KeepAlive: %s", err)
+	}
+
+	if remoteUDPResolver == nil && remoteTCPResolver == nil {
+		log.Printf("KeepAlive: No remote resolver available")
+		return
 	}
 
 	for {
-		_, err := remoteUDPResolver.LookupIP(context.Background(), "ip4", "www.baidu.com")
-		if err != nil {
-			log.DebugPrintf("KeepAlive using UDP error: %s", err)
+		useTCP := false
 
-			// Try using TCP resolver
-			_, err = remoteTCPResolver.LookupIP(context.Background(), "ip4", "www.baidu.com")
+		if remoteUDPResolver != nil {
+			_, err := remoteUDPResolver.LookupIP(context.Background(), "ip4", "www.baidu.com")
 			if err != nil {
-				log.Printf("KeepAlive: %s", err)
+				log.DebugPrintf("KeepAlive using UDP error: %s", err)
+				useTCP = true
 			} else {
-				log.Printf("KeepAlive: OK")
+				log.Printf("KeepAlive using UDP: OK")
 			}
-		} else {
-			log.Printf("KeepAlive: OK")
+		}
+
+		if useTCP && remoteTCPResolver != nil {
+			_, err := remoteTCPResolver.LookupIP(context.Background(), "ip4", "www.baidu.com")
+			if err != nil {
+				log.Printf("KeepAlive using TCP error: %s", err)
+			} else {
+				log.Printf("KeepAlive using TCP: OK")
+			}
 		}
 
 		time.Sleep(60 * time.Second)
