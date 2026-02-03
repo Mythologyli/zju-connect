fc49f93cced3680807ba24d38aa2dfbe80eb70c5
Myth <myth@myth.cx>
2026-02-03 23:30:07 +0800

    chore: bump version to v1.0.0-beta.4

--- init.go
+++ init.go
@@ -18,7 +18,7 @@ import (
 
 var CommitID string
 
-const zjuConnectVersion = "1.0.0-beta.3"
+const zjuConnectVersion = "1.0.0-beta.4"
 
 func getTOMLVal[T int | uint64 | string | bool](valPointer *T, defaultVal T) T {
 	if valPointer == nil {
